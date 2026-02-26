192ef0b75785f1d340affb30b94ae2db113ff222
nubrixstudio <nubrixstudio@gmail.com>
2026-02-26 17:44:14 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,24 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-```typescript
-    base: './',
-```
-    plugins: [react()],
-    resolve: {
+  base: './',
+  plugins: [react()],
+  resolve: {
     alias: {
-        '@': path.resolve(__dirname, './src_final'),
+      '@': path.resolve(__dirname, './src_final'),
     },
-},
-    server: {
+  },
+  server: {
     watch: {
-        ignored: [
-            '**/src/**',
-            '**/src_broken/**',
-            '**/src_corrupted_2/**',
-            '**/src_corrupted_3/**',
-            '**/src_corrupted_4/**',
-            '**/src_corrupted_5/**',
-  
+      ignored: [
+        '**/src/**',
+        '**/src_broken/**',
+        '**/src_corrupted_2/**',
+        '**/src_corrupted_3/**',
+        '**/src_corrupted_4/**',
+        '**/src_corrupted_5/**',
+        '**/src_corrupted_6/**'
+      ]
+    }
+  }
+})
